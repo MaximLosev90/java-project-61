3fe1a9fdb4987f53a69ce04903e85fb483f0db58
Maxim Losev <maximlosev90@gmail.com>
2023-12-13 20:18:13 +0400

    fix App.java and Cli.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,19 @@
 package hexlet.code;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        Cli.greeting();
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        // System.out.println("2 - Even");
+        System.out.println("0 - Exit");
+        System.out.print("Your choice: ");
+        Scanner choice = new Scanner(System.in);
+        String userChoice = choice.next();
+        if (userChoice.equals("1")) {
+            System.out.println("Welcome to the Brain Games!");
+            Cli.greeting();
+            choice.close();
+        }
     }
 }
