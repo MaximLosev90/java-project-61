ea1651db390aaf4ef8fb84b165b30eb42e1b8bb6
Maxim Losev <maximlosev90@gmail.com>
2023-12-14 10:54:51 +0400

    create Even.java, fix App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,17 +3,15 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Please enter the game number and press Enter.");
-        System.out.println("1 - Greet");
-        // System.out.println("2 - Even");
-        System.out.println("0 - Exit");
-        System.out.print("Your choice: ");
+        System.out.print("Please enter the game number and press Enter.\n1 - Greet\n2 - Even\n0 - Exit\nYour choice: ");
         Scanner choice = new Scanner(System.in);
         String userChoice = choice.next();
+        System.out.println("Welcome to the Brain Games!");
         if (userChoice.equals("1")) {
-            System.out.println("Welcome to the Brain Games!");
             Cli.greeting();
-            choice.close();
+        } else if (userChoice.equals("2")) {
+            Even.evenGame();
         }
+        choice.close();
     }
 }
---
+++ app/src/main/java/hexlet/code/Even.java
@@ -0,0 +1,40 @@
+package hexlet.code;
+import java.util.Scanner;
+
+public class Even {
+    public static void evenGame() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("May I have your name? ");
+        String userName = scanner.next();
+        System.out.println("Hello, " + userName + "!");
+        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
+        int i = 1;
+        while (i < 4) {
+            int randomNumber = (int) (Math.random() * 99 + 1);
+            System.out.println("Question: " + randomNumber);
+            Scanner answer = new Scanner(System.in);
+            System.out.print("Your answer: ");
+            String userAnswer = answer.next();
+            String correctAnswer;
+            if (randomNumber % 2 == 0) {
+                correctAnswer = "yes";
+            } else {
+                correctAnswer = "no";
+            }
+            if (randomNumber % 2 == 0 && userAnswer.equalsIgnoreCase("yes")) {
+                System.out.println("Correct!");
+                i++;
+            } else if (randomNumber % 2 != 0 && userAnswer.equalsIgnoreCase("no")) {
+                System.out.println("Correct!");
+                i++;
+            } else {
+                System.out.println("'" + userAnswer + "'" + " is wrong answer ;(. Correct answer was "
+                        + "'" + correctAnswer + "'.\n" + "Let's try again, " + userName + "!");
+                break;
+            }
+            if (i == 4) {
+                System.out.println("Congratulations, " + userName + "!");
+            }
+        }
+    }
+}
